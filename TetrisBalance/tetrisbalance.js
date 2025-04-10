e9ea60d88db06e35fca17b1ce6418e1198e6a414
Frank <ChrnoDarko>
2025-04-09 20:04:42 -0600

    Added new folder to separate games, starting html on tetris

--- TetrisBalance/tetrisbalance.js
+++ TetrisBalance/tetrisbalance.js
@@ -1,3 +1,5 @@
+const startGameBtn = document.getElementById('startGameBtn');
+
 let tetrominoSet =
 ['I','I','I','I',
  'O','O','O','O',
@@ -11,15 +13,22 @@ let remainingSet;
 
 let currentPiece;
 let previousPiece;
+let score = -1;
 
 function setup(){
     remainingSet = tetrominoSet;    
 }
 
 function selectTretomino(){
+    placedPieces++;
     if(remainingSet.length === 0){return}
     let randomNum = Math.floor(Math.random() * remainingSet.length);
     
     previousPiece = currentPiece;
     currentPiece = remainingSet.splice(randomNum,1);
+}
+
+startGameBtn.onclick = function(){
+    setup();
+    selectTretomino();
 }
