4db5aa460a09e3a3f604efb9eb9c43ae5a0dc2f0
Frank <ChrnoDarko>
2025-04-11 12:59:21 -0600

    Functional Tetris

--- TetrisBalance/tetrisbalance.js
+++ TetrisBalance/tetrisbalance.js
@@ -1,34 +1,56 @@
-const startGameBtn = document.getElementById('startGameBtn');
+const SelectAPieceBtn = document.getElementById('SelectAPieceBtn');
+const StartGameBtn = document.getElementById('StartGameBtn');
+const scoreP = document.getElementById('Score');
+const currentPieceImg = document.getElementById('CurrentPieceImg');
+const previousPieceImg = document.getElementById('PreviousPieceImg');
 
-let tetrominoSet =
+const tetrominoSet =
 ['I','I','I','I',
- 'O','O','O','O',
- 'T','T','T','T',
- 'S','S','S','S',
- 'Z','Z','Z','Z',
  'J','J','J','J',
- 'L','L','L','L'];
+ 'L','L','L','L',
+ 'O','O','O','O',
+ 'S','S','S','S',
+ 'T','T','T','T',
+ 'Z','Z','Z','Z'];
 
 let remainingSet;
 
 let currentPiece;
 let previousPiece;
-let score = -1;
+let score = 0;
 
 function setup(){
-    remainingSet = tetrominoSet;    
+    score = 0;
+    //remainingSet = tetrominoSet;
+    remainingSet = [].concat(tetrominoSet);
+    previousPieceImg.src ="";
+    console.log(tetrominoSet);
+    console.log(remainingSet);
 }
 
 function selectTretomino(){
-    placedPieces++;
     if(remainingSet.length === 0){return}
+    score++;
     let randomNum = Math.floor(Math.random() * remainingSet.length);
     
-    previousPiece = currentPiece;
+    if(currentPiece != null){previousPiece = currentPiece;}
     currentPiece = remainingSet.splice(randomNum,1);
+
+    console.log(tetrominoSet);
+    console.log(remainingSet);
 }
 
-startGameBtn.onclick = function(){
+StartGameBtn.onclick = function(){
     setup();
     selectTretomino();
+    currentPieceImg.src = "TetrominoImages/"+currentPiece+".png";
+    SelectAPieceBtn.hidden = false;
+    scoreP.innerHTML = score;
+}
+
+SelectAPieceBtn.onclick = function(){
+    selectTretomino();
+    scoreP.innerHTML = score;
+    currentPieceImg.src = "TetrominoImages/"+currentPiece+".png";
+    previousPieceImg.src = "TetrominoImages/"+previousPiece+".png";
 }
